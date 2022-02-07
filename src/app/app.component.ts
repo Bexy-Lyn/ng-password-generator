55dfe1dbafb815993cc3d81a631fee53a7479e37
Rebecca Otto <rebecca.otto@eyeworkers.de>
2022-02-07 17:38:13 +0100

    password generation functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,76 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  length = 24;
+  lower = true;
+  upper = false;
+  numbers = false;
+  symbols = false;
+
+  password = '';
+
+  setLength(target: any) {
+    if (target.value >= 4) this.length = target.value;
+    else this.length = 4;
+  }
+
+  toggleLower() {
+    this.lower = !this.lower;
+  }
+
+  toggleUpper() {
+    this.upper = !this.upper;
+  }
+
+  toggleNumbers() {
+    this.numbers = !this.numbers;
+  }
+
+  toggleSymbols() {
+    this.symbols = !this.symbols;
+  }
+
+  private getRandom(str: string) {
+    const index = Math.floor(Math.random() * str.length);
+    return str[index];
+  }
+
+  onButtonClick() {
+    const lower = 'abcdefghijklmnopqrstuvwxyz';
+    const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()/§';
+    let l = this.length;
+
+    let generatedPw = '';
+    let validChars = '';
+    if (this.lower) {
+      validChars += lower;
+      generatedPw += this.getRandom(lower);
+      l--;
+    }
+    if (this.upper) {
+      validChars += upper;
+      generatedPw += this.getRandom(upper);
+      l--;
+    }
+    if (this.numbers) {
+      validChars += numbers;
+      generatedPw += this.getRandom(numbers);
+      l--;
+    }
+    if (this.symbols) {
+      validChars += symbols;
+      generatedPw += this.getRandom(symbols);
+      l--;
+    }
+
+    for (let i = 0; i < l; i++) {
+      generatedPw += this.getRandom(validChars);
+    }
+    this.password = generatedPw;
+  }
 }
